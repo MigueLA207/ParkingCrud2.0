2d15d3d1c3f2c619d9e7a0da4ab61b9f47b30c9c
Miguelito <mariasmarin981@gmail.com>
2025-10-20 19:59:47 -0500

    fix date.now

--- CrudPark.Api/obj/Debug/net9.0/CrudPark.Api.AssemblyInfo.cs
+++ CrudPark.Api/obj/Debug/net9.0/CrudPark.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudPark.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44edb9461d6b6204b19730283e219bd4c62bd606")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d713788ec64d2f433b6abbe40a70b2ff25b34e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudPark.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudPark.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
